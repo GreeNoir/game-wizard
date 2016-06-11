d69a4b7bf9fca0e4d394cc371195161f20973fab
GreeNoir <ametist3g@gmail.com>
2016-06-11 23:55:13 +0300

    trigger

--- triggers.sql
+++ triggers.sql
@@ -4,26 +4,20 @@ UPDATE tmpItem1 SET OwnerID=300, SerialNum=500089810450
 
 DELIMITER $$
 
-CREATE
-/*[DEFINER = { user | CURRENT_USER }]*/
-TRIGGER `sm_db`.`item_insert` BEFORE INSERT
-ON `sm_db`.`item` FOR EACH ROW
+CREATE TRIGGER `sm_db`.`item_insert` BEFORE INSERT 
+ON `sm_db`.`item` FOR EACH ROW 
 BEGIN
-
-DROP TABLE tblTmpAccountInfo IF EXISTS $$
-CREATE TABLE tblTmpAccountInfo
-SELECT account_common.AccountID AS AccountID,
-    account_common.BaiBaoYuanBao AS BaiBao
-    FROM account_common
-    INNER JOIN roledata
-      ON account_common.AccountID = roledata.AccountID
-  WHERE NEW.OwnerID = roledata.RoleID $$
-
-
-IF tblTmpAccountInfo.BaiBao = 0
-THEN CALL `'Cannot add row'`  $$
-
-  END IF $$
-END $$
+  IF 
+  (SELECT 
+    `sm_db`.`account_common`.BaiBaoYuanBao 
+  FROM
+    `sm_db`.`account_common` 
+    INNER JOIN `sm_db`.`roledata` 
+      ON `sm_db`.`account_common`.AccountID = `sm_db`.`roledata`.AccountID 
+  WHERE NEW.OwnerID = `sm_db`.`roledata`.RoleID) = 0 
+  THEN SIGNAL SQLSTATE '01000' SET MESSAGE_TEXT = 'Error: insert row';
+  
+    END IF;
+END$$
 
 DELIMITER ;
